a366a6ec57625ab877108622e1de5332e88ad9a0
Harley D. Eades III <harley.eades@gmail.com>
2023-01-01 20:35:33 -0500

    Defined Maybe<T> and then started parseTerm.

--- parser.ts
+++ parser.ts
@@ -5,4 +5,34 @@ const ohm = require('ohm-js');
 const contents = fs.readFileSync('Grammar.ohm', 'utf-8');
 const grammar = ohm.grammar(contents);
 
-console.log(grammar.match("fun x { x }", "term").succeeded())
+interface Just<T> {
+    kind: "just"
+    value: T
+}
+
+function mkJust<T>(e: T): Just<T> {
+    return { kind: "just", value: e }
+}
+
+interface Nothing {
+    kind: "nothing"
+}
+
+function mkNothing(): Nothing {
+    return { kind: "nothing" }
+}
+
+type Maybe<T> = Just<T> | Nothing
+
+export function parseTerm(e: string): Maybe<Syntax.Term> {
+    let matcher = grammar.match(e, "term");
+
+    if (matcher.succeeded()) {
+        // Parse e into a term t and return that.
+        return mkJust(Syntax.mkVar("x"))
+    } else {
+        return mkNothing()
+    }
+}
+
+console.log(grammar.match("(fun x { x y }) z", "term").succeeded())
